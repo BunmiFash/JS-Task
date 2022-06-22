e93d4ccadc00af83c86e59052d95da03a387351f
Bunmi <foluwaboonmy@gmail.com>
2022-06-22 12:00:40 -0700

    A program that displays my name, height and country

---
+++ index.js
@@ -0,0 +1,9 @@
+var fname;
+fname = "Bunmi"
+height = "5ft5"
+country = "Nigeria"
+
+
+document.getElementById("name").innerHTML = "My name is" + " " +fname + ".";
+document.getElementById("height").innerHTML = "I'm" + " " + height + " " + "tall.";
+document.getElementById("country").innerHTML = "I'm from" + " " + country + ".";
