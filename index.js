8754d70eb8e4fa6833d59dabe92a79ab932b0419
Bunmi <foluwaboonmy@gmail.com>
2022-06-22 20:54:51 -0700

    'Name, Height and Country display'

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var fname;
-fname = "Bunmi"
-height = "5ft5"
-country = "Nigeria"
+
+var fname = "Bunmi"
+var height = "5ft5"
+var country = "Nigeria"
 
 
-document.getElementById("name").innerHTML = "My name is" + " " +fname + ".";
-document.getElementById("height").innerHTML = "I'm" + " " + height + " " + "tall.";
-document.getElementById("country").innerHTML = "I'm from" + " " + country + ".";
+alert("Hi there! I'm" +" "+ fname + ".")
+alert("I'm" + " " + height + " "+ "tall.")
+alert("I'm from" + " " + country + ".")
+
